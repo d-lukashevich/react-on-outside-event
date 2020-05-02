70183593d1bd0e76c4c17fd21cfeadd4e3445cea
Denis Lukashevich <denis.lukashevich@megafon.ru>
2020-05-02 17:12:38 +0300

    build(project): adjust building to exact hook

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > Tools for detecting events outside of VDOM subtree, with portals support
 
-[![NPM](https://img.shields.io/npm/v/react-on-outside-event.svg)](https://www.npmjs.com/package/react-on-outside-event) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![NPM](https://img.shields.io/npm/v/react-on-outside-event.svg)](https://www.npmjs.com/package/react-on-outside-event)
 
 ## Install
 
@@ -13,18 +13,14 @@ npm install --save react-on-outside-event
 ## Usage
 
 ```tsx
-import * as React from 'react'
+import * as React from 'react';
 
-import { useMyHook } from 'react-on-outside-event'
+import { useMyHook } from 'react-on-outside-event';
 
 const Example = () => {
-  const example = useMyHook()
-  return (
-    <div>
-      {example}
-    </div>
-  )
-}
+    const example = useMyHook();
+    return <div>{example}</div>;
+};
 ```
 
 ## License
@@ -33,4 +29,4 @@ MIT © [d-lukashevich](https://github.com/d-lukashevich)
 
 ---
 
-This hook is created using [create-react-hook](https://github.com/hermanya/create-react-hook).
+These tools are created using [create-react-hook](https://github.com/hermanya/create-react-hook).
--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,35 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-
-import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
 
 export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
+    input: {
+        index: 'src/index.tsx',
+        useOutsideEvent: 'src/useOutsideEvent.tsx'
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    url({ exclude: ['**/*.svg'] }),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    commonjs()
-  ]
-}
+    output: [
+        {
+            dir: 'dist',
+            format: 'cjs',
+            exports: 'named'
+        },
+        {
+            dir: 'dist/esm',
+            format: 'es',
+            exports: 'named'
+        }
+    ],
+    plugins: [
+        external(),
+        url({ exclude: ['**/*.svg'] }),
+        resolve(),
+        typescript({
+            useTsconfigDeclarationDir: true,
+            rollupCommonJSResolveHack: true,
+            clean: true
+        }),
+        commonjs()
+    ]
+};
