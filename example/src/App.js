75185cd3c28e9b2bb358e23b9ad3909b7f7c2c8b
Denis Lukashevich <denis.lukashevich@megafon.ru>
2020-06-07 03:08:44 +0700

    docs(example): add example of OutsideEventContainer

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { useOutsideEvent } from 'react-on-outside-event';
+import { useOutsideEvent, OutsideEventContainer } from 'react-on-outside-event';
 
 const App = () => {
     const handler = useOutsideEvent(() => {
-        console.log('click-outside');
+        console.log('click outside of useOutsideEvent using div');
     });
     const extraHandler = () => {
-        console.log('my Extra');
+        console.log('extra handler of useOutsideEvent');
+    };
+    const extraHandler2 = () => {
+        console.log('extra handler of OutsideEventContainer');
     };
     return (
-        <div onClick={handler(extraHandler)} style={{ width: 100, height: 100, background: 'red' }}>
-            {ReactDOM.createPortal(
-                <div style={{ width: 100, height: 100, background: 'blue' }}>Test with portal</div>,
-                document.body
-            )}
-            Test box
+        <div style={{ display: 'flex' }}>
+            <div onClick={handler(extraHandler)} style={{ width: 200, height: 200, background: 'red', padding: 5 }}>
+                {ReactDOM.createPortal(
+                    <div style={{ width: 200, height: 200, background: 'blue', padding: 5 }}>
+                        useOutsideEvent: portal part
+                    </div>,
+                    document.body
+                )}
+                Div with useOutsideEvent
+            </div>
+            <OutsideEventContainer
+                onClick={extraHandler2}
+                callback={() => {
+                    console.log('click outside of OutsideEventContainer');
+                }}
+                style={{ width: 200, height: 200, background: 'green', padding: 5 }}>
+                {ReactDOM.createPortal(
+                    <div style={{ width: 200, height: 200, background: 'yellow', padding: 5 }}>
+                        OutsideEventContainer: portal part
+                    </div>,
+                    document.body
+                )}
+                Div with OutsideEventContainer
+            </OutsideEventContainer>
         </div>
     );
 };
