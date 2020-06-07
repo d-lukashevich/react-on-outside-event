48016de955bc3b210afcb52e368859ed250662e7
Denis Lukashevich <denis.lukashevich@megafon.ru>
2020-06-07 18:32:17 +0700

    feat(OutsideEventContainer): add ref forwarding

--- src/OutsideEventContainer.ts
+++ src/OutsideEventContainer.ts
@@ -1,4 +1,4 @@
-import { createElement } from 'react';
+import { createElement, forwardRef } from 'react';
 import useOutsideEvent from './useOutsideEvent';
 import { EventHandlerName } from './typings';
 
@@ -9,16 +9,23 @@ export interface OutsideEventContainerProps {
     [key: string]: any;
 }
 
-export const OutsideEventContainer = ({
-    callback,
-    eventHandlerName = 'onClick',
-    [eventHandlerName]: additionalHandler,
-    element = 'div',
-    ...restProps
-}: OutsideEventContainerProps) => {
-    const batter = useOutsideEvent(callback, eventHandlerName);
-    const syntheticHandler = typeof additionalHandler !== 'function' ? batter : batter(additionalHandler);
-    return createElement(element, { [eventHandlerName]: syntheticHandler, ...restProps });
-};
+const OutsideEventContainer = forwardRef(
+    (
+        {
+            callback,
+            eventHandlerName = 'onClick',
+            [eventHandlerName]: additionalHandler,
+            element = 'div',
+            ...restProps
+        }: OutsideEventContainerProps,
+        ref
+    ) => {
+        const batter = useOutsideEvent(callback, eventHandlerName);
+        const syntheticHandler = typeof additionalHandler !== 'function' ? batter : batter(additionalHandler);
+        return createElement(element, { [eventHandlerName]: syntheticHandler, ref, ...restProps });
+    }
+);
+OutsideEventContainer.displayName = 'OutsideEventContainer';
 
+export { OutsideEventContainer };
 export default OutsideEventContainer;
